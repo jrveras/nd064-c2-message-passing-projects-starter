f7ae9d5657afcea90f92f3ca68d22e69b0857915
Jairton Junior <jairton.araujojr@gmail.com>
2021-11-28 18:22:09 -0300

    update consumer feature

--- modules/person_api/app/udaconnect/controllers.py
+++ modules/person_api/app/udaconnect/controllers.py
@@ -22,7 +22,7 @@ DATE_FORMAT = "%Y-%m-%d"
 
 api = Namespace("UdaConnect", description="Connections via geolocation.")  # noqa
 config = {'bootstrap.servers': 'my-release-kafka-0.my-release-kafka-headless.default.svc.cluster.local:9092'}
-configConsumer = {'bootstrap.servers': 'my-release-kafka.default.svc.cluster.local:9092', 'group.id': 'python_example_group_1', 'auto.offset.reset': 'largest'}
+configConsumer = {'bootstrap.servers': 'my-release-kafka.default.svc.cluster.local:9092', 'group.id': 'python_example_group_1', 'auto.offset.reset': 'latest'}
 topic = "person_queue"
 # Parse the command line.
 # parser = ArgumentParser()
