27935389b90314a4caa6763467148d892090f0d8
Jairton Junior <jairton.araujojr@gmail.com>
2021-11-28 16:40:38 -0300

    update consumer feature

--- modules/person_api/app/udaconnect/controllers.py
+++ modules/person_api/app/udaconnect/controllers.py
@@ -22,8 +22,9 @@ DATE_FORMAT = "%Y-%m-%d"
 
 api = Namespace("UdaConnect", description="Connections via geolocation.")  # noqa
 config = {'bootstrap.servers': 'my-release-kafka-0.my-release-kafka-headless.default.svc.cluster.local:9092'}
-configConsumer = {'bootstrap.servers': 'my-release-kafka.default.svc.cluster.local:9092', 'group.id': 'python_example_group_1', 'auto.offset.reset': 'latest'}
-topic = "people"
+# configConsumer = {'bootstrap.servers': 'my-release-kafka.default.svc.cluster.local:9092', 'group.id': 'python_example_group_1', 'enable.auto.commit': True, 'enable.auto.offset.store': False, 'socket.timeout.ms': 50, 'session.timeout.ms': 100, 'auto.offset.reset': 'latest'}
+configConsumer = {'bootstrap.servers': 'my-release-kafka.default.svc.cluster.local:9092', 'group.id': 'python_example_group_1', 'enable.auto.commit': True, 'enable.auto.offset.store': False, 'socket.timeout.ms': 50, 'session.timeout.ms': 100}
+topic = "person_queue"
 # Parse the command line.
 # parser = ArgumentParser()
 # parser.add_argument('--reset', action='store_true')
