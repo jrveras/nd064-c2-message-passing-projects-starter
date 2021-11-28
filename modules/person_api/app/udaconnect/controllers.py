59d26b829b7833cea8ca6a5e5e6fe564a1be94cf
Jairton Junior <jairton.araujojr@gmail.com>
2021-11-28 19:03:34 -0300

    update consumer feature

--- modules/person_api/app/udaconnect/controllers.py
+++ modules/person_api/app/udaconnect/controllers.py
@@ -15,7 +15,6 @@ from flask_accepts import accepts, responds
 from flask_restx import Namespace, Resource
 from typing import Optional, List
 from confluent_kafka import Producer
-# from argparse import ArgumentParser, FileType
 from confluent_kafka import Consumer, OFFSET_BEGINNING
 
 DATE_FORMAT = "%Y-%m-%d"
@@ -23,12 +22,8 @@ DATE_FORMAT = "%Y-%m-%d"
 api = Namespace("UdaConnect", description="Connections via geolocation.")  # noqa
 config = {'bootstrap.servers': 'my-release-kafka-0.my-release-kafka-headless.default.svc.cluster.local:9092'}
 # configConsumer = {'bootstrap.servers': 'my-release-kafka.default.svc.cluster.local:9092', 'group.id': 'python_example_group_1', 'auto.offset.reset': 'latest'}
-configConsumer = {'bootstrap.servers': 'my-release-kafka.default.svc.cluster.local:9092', 'group.id': 'python_example_group_1'}
+configConsumer = {'bootstrap.servers': 'my-release-kafka.default.svc.cluster.local:9092', 'group.id': 'python_example_group_1', 'enable.auto.commit': False, 'auto.offset.reset': 'earliest'}
 topic = "person_queue_2"
-# Parse the command line.
-# parser = ArgumentParser()
-# parser.add_argument('--reset', action='store_true')
-# args = parser.parse_args()
 
 class _ExcludeErrorsFilter(logging.Filter):
     def filter(self, record):
@@ -104,32 +99,31 @@ class PersonsResource(Resource):
         p = json.dumps(payload)
 
         producer.produce(topic, p)
-        producer.poll(1.0)
+        # producer.poll(10000)
         producer.flush()
-
-        # logger.debug('WARNING: Message args 3: {}'.format(payload))
-        # logger.debug('WARNING: TRICOLOR')
 
         # Create Consumer instance
         consumer = Consumer(configConsumer)
 
         # Set up a callback to handle the '--reset' flag.
-        def reset_offset(consumer, partitions):
-            # if args.reset:
-            for p in partitions:
-                p.offset = OFFSET_BEGINNING
-            consumer.assign(partitions)
+        # def reset_offset(consumer, partitions):
+        #     # if args.reset:
+        #     for p in partitions:
+        #         p.offset = OFFSET_BEGINNING
+        #     consumer.assign(partitions)
 
         # Subscribe to topic
-        consumer.subscribe([topic], on_assign=reset_offset)
-        # consumer.subscribe([topic])
+        consumer.subscribe([topic])
+        # msg = consumer.poll(1.0)
+        msg = consumer.poll(timeout=1.0)
 
-        msg = consumer.poll(1.0)
         result = msg.value().decode("utf-8")
         logger.debug('WARNING: Resulta: {}'.format(result))
         np = json.loads(result)
         logger.debug('WARNING: New Person: {}'.format(np))
         new_person: Person = PersonService.create(np)
+        
+        consumer.commit(asynchronous=False)
         consumer.close()
 
         # new_person: Person = PersonService.create(payload)
